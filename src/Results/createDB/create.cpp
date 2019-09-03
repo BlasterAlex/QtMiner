25c5a760656dafaa55da9f361f840640c81b4dce
Alexander <pichulev23071999@gmail.com>
2019-09-03 16:39:15 +0400

    Fixed the logic of the db table creation program

--- src/Results/createDB/create.cpp
+++ src/Results/createDB/create.cpp
@@ -7,25 +7,24 @@
 int main() {
 
   QSqlDatabase db = connectDB("../connection/config");
-
   if (db.open()) {
 
     QSqlQuery query;
-
     if (query.exec(
             "CREATE TABLE public.results(id SERIAL, date DATE NOT NULL, width INTEGER NOT NULL, height INTEGER NOT NULL, mines INTEGER NOT NULL, "
             "totalTime TIME NOT NULL);"
             "ALTER TABLE ONLY public.results "
             "ADD CONSTRAINT mines_val CHECK (mines < width*height);")) {
       qDebug() << "Ok";
+      db.close();
+      return 0;
     } else {
       qDebug() << "Произошла ошибка при создании таблицы";
+      db.close();
+      return 0;
     }
-
-    db.close();
-  } else {
-    qDebug() << "Ошибка соединения с бд";
   }
 
+  qDebug() << "Ошибка соединения с бд";
   return 1;
 }
